cc03158f1199f6c7312716afd2e0af3babe7ca18
Megan <penn.meg@gmail.com>
2018-05-02 21:43:24 -0400

    hover challenge using mouse enter mouse lvea

--- scripts.js
+++ scripts.js
@@ -1,11 +1,24 @@
-var yellowButton = document.getElementById("buttonOne");
-var purpleButton = document.getElementById("buttonTwo");
-var buttonText = document.getElementById("buttonText");
-yellowButton.addEventListener("click", function(e){
-  console.log("--- yellow button clicked ---");
-  buttonText.innerHTML = "I'm right";
-});
-purpleButton.addEventListener("click", function(e){
-  console.log("--- purple button clicked ---");
-  buttonText.innerHTML = "No I'm Right";
+$(document).ready(function () {
+  // Talking Buttons Challenge
+  var yellowButton = document.getElementById("buttonOne");
+  var purpleButton = document.getElementById("buttonTwo");
+  var buttonText = document.getElementById("buttonText");
+  yellowButton.addEventListener("click", function(e){
+    console.log("--- yellow button clicked ---");
+    buttonText.innerText = "I'm right";
+  });
+  purpleButton.addEventListener("click", function(e){
+    console.log("--- purple button clicked ---");
+    buttonText.innerText = "No I'm Right";
+  });
+  // Hover Challenge
+  $("#hoverBox").mouseenter(function(e){
+    console.log("=== mouse hovers over box ===");
+    hoverBox.innerHTML = "<p>Hey, I told you not to hover over me!</p>";
+    hoverBox.style = "background-color: purple";
+  })
+  .mouseleave(function(e){
+    hoverBox.innerHTML = "<p>Do not hover over me.</p>";
+    hoverBox.style = "background-color: red";
+  });
 });
